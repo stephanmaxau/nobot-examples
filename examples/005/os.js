870779f22d24814373bf34746e37f89b82f8c52b
Stephan <stephan@maxau.de>
2022-01-05 09:26:07 +0100

    example 5 done - references can be found here: https://nodejs.org/docs/latest-v10.x/api/

--- examples/005/os.js
+++ examples/005/os.js
@@ -1,1 +1,19 @@
 const os = require('os');
+
+const homeDirectory = os.homedir();
+console.log(`Your ho,e directory is: ${homeDirectory}`);
+
+const osPlatform = os.platform();
+console.log(`The OS platform is: ${osPlatform}`);
+
+const osArchitecture = os.arch();
+console.log(`The architecture of your computer is: ${osArchitecture}`);
+
+const cpuCores = os.cpus();
+const coreCount = cpuCores.length;
+const cpuModel = cpuCores[0].model;
+
+console.log(`I can see your : ${cpuModel} has ${coreCount} cores`);
+
+const hostName = os.hostname();
+console.log(`Your hostname is: ${hostName}`);
