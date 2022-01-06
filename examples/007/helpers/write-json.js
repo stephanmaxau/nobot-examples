9fbc106b8936583d2345ce15e1915f5bc8b9af14
Stephan <stephan@maxau.de>
2022-01-06 14:14:33 +0100

    example 7 done - wondering about lots of different possibilities e.g. to declare functions

--- examples/007/helpers/write-json.js
+++ examples/007/helpers/write-json.js
@@ -1,1 +1,14 @@
 const fs = require('fs');
+
+const JSON_WHITESPACE = 4;
+
+const writeJson = (file, contents) => new Promise((resolve, reject) => {
+  fs.writeFile(file, JSON.stringify(contents, null, JSON_WHITESPACE), (err) => {
+    if (err) {
+      reject(err);
+    }
+    resolve(`${file} written`);
+  });
+});
+
+module.exports = writeJson;
---
+++ examples/007/read-json.js
@@ -0,0 +1,5 @@
+const { projectId, startDate, endDate } = require('./data/example-config');
+
+console.log(`The ID of the project is: ${projectId}`);
+console.log(`The start date  of the project is: ${startDate}`);
+console.log(`The end date of the project is: ${endDate}`);
