1ee45c3185f2727831a1e301a5c45686a7e754f7
Stephan <stephan@maxau.de>
2022-01-06 14:31:12 +0100

    example 8 done

--- examples/008/zip.js
+++ examples/008/zip.js
@@ -1,3 +1,40 @@
 const archiver = require('archiver');
 const fs = require('fs');
 const path = require('path');
+
+const ZLIB_BEST_COMPRESSION = 9;
+
+// create a file to stream archive data to:
+
+const zipPath = path.join(__dirname, 'files.zip');
+
+const output = fs.createWriteStream(zipPath);
+const archive = archiver('zip', {
+  zlip: { level: ZLIB_BEST_COMPRESSION}
+});
+
+// listen for all archive data to be written:
+
+output.on('close', () => {
+  console.log(`Total bytes: ${archive.pointer()}`);
+  console.log('archiving has now finished.');
+});
+
+// good practice is to catch this error explicitly:
+
+archive.on('error', (err) => {
+  throw err;
+});
+
+archive.pipe(output);
+
+// add files (read the copy.txt and logo.jpg and output with different names):
+
+const textPath = path.join(__dirname, 'copy.txt');
+const logoPath = path.join(__dirname, 'logo.jpg');
+archive.append(fs.createReadStream(textPath), { name: 'context.txt' });
+archive.append(fs.createReadStream(logoPath), { name: 'nobot.jpg' });
+
+// finalize teh archive (ie we are done appending files bit streams have to finish yet):
+
+archive.finalize();
